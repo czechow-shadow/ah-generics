acfa110072d62c0deb27db48b467139ee78236d1
czechow <pawel.czechowicz@vp.pl>
2019-07-10 10:41:27 +0200

    First working version

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
@@ -13,7 +14,7 @@ module Lib where
 
 import Protolude
 import GHC.Generics
-import qualified Data.Text as T
+-- import qualified Data.Text as T
 import qualified Data.Generics as G
 
 
@@ -33,7 +34,7 @@ newtype Url = Url { unUrl :: Text } deriving (Show, G.Data)
 newtype EncrText = EncrText { unEncrText :: Text } deriving (Show, G.Data)
 
 type ConfigResolved = ConfigF Password Url EncrText
-  
+
 change :: ConfigResolved -> ConfigResolved
 change c = G.everywhere (G.mkT (chgPwd "...")) .
            G.everywhere (G.mkT (chgUrl "---")) $ c
@@ -62,7 +63,7 @@ config = Config { clients =
                 , key = Key $ EncrText "klucz"
                 }
 
-  
+
 data Tree a = Leaf a | Node (Tree a) (Tree a)
   deriving (Generic, Show, G.Data)
 
@@ -70,13 +71,13 @@ type TreeInt = Tree Int
 
 t :: TreeInt
 t = Leaf 3
-  
+
 go :: IO ()
 go = do
   putText $ show $ G.everywhere (\x -> x) t
   putText "End"
 
-  
+
   putText "===> End"
 
 incrV :: Int -> TreeInt -> TreeInt
@@ -103,7 +104,7 @@ instance ( Generic s
 
 class GDoJob s a where
   gDoJob :: forall g (x :: Type) . Applicative g => (a -> g a) -> s x -> g (s x)
-  
+
 instance (GDoJob l a, GDoJob r a) => GDoJob (l :*: r) a where
   gDoJob f (l :*: r) = (:*:) <$> gDoJob f l <*> gDoJob f r
 
@@ -117,11 +118,11 @@ instance GDoJob s a => GDoJob (M1 m meta s) a where
 instance GDoJob U1 a where
   gDoJob _ x = pure x
 
-instance {-# OVERLAPPABLE #-} (Show a, DoJob (h (g (f a))) a)
+instance {-# OVERLAPPING #-} (Show a, DoJob (h (g (f a))) a)
       => GDoJob (K1 R (h (g (f a)))) a where
   gDoJob f (K1 xs) = K1 <$> doJob f xs
 
-instance {-# OVERLAPPABLE #-} (Show a, DoJob (g (f a)) a) => GDoJob (K1 R (g (f a))) a where
+instance {-# OVERLAPPING #-} (Show a, DoJob (g (f a)) a) => GDoJob (K1 R (g (f a))) a where
   gDoJob f (K1 xs) = trace (("Found [[a]] in " :: Text)) $ -- <> show xs) $
                      K1 <$> doJob f xs
 
@@ -131,39 +132,57 @@ instance {-# OVERLAPPABLE #-} (Show a, DoJob (g (f a)) a) => GDoJob (K1 R (g (f 
 --     let z = doJob f xs
 --         z' = K1 <$> z
 --     in z'
-instance {-# OVERLAPPABLE #-} (Show (f a), DoJob (f a) a)
+instance {-# OVERLAPPING #-} (Show (f a), DoJob (f a) a)
       => GDoJob (K1 R (f a)) a where
   gDoJob f (K1 xs) = trace (("Found [a] in [" :: Text) <> show xs) $
                      K1 <$> doJob f xs
 
+type family IfTypesEq (t1 :: Type) (t2 :: Type) :: Bool where
+  IfTypesEq a a = 'True
+  IfTypesEq _ _ = 'False
 
-instance {-# OVERLAPPING #-} (Show a) => GDoJob (K1 R a) a where
-  gDoJob f (K1 x) = K1 <$> f x
+
+-- instance {-# OVERLAPPING #-} (Show a) => GDoJob (K1 R a) b where
+--   gDoJob f (K1 x) = K1 <$> pure x -- K1 <$> f x
+instance {-# OVERLAPPING #-} (Show a, teq ~ (IfTypesEq a b),  GDoJobRec teq (K1 R a) b)
+       => GDoJob (K1 R a) b where
+  -- gDoJob f (K1 x) = K1 <$> pure x -- K1 <$> f x
+  gDoJob f (K1 x) = gDoJobRec (Proxy :: Proxy teq) f (K1 x)
     -- panic $ "Found int type equal type in " <> show x
 
-class GDoJobRec 
--- class GDoJob s a where
---   gDoJob :: forall g (x :: Type) . Applicative g => (a -> g a) -> s x -> g (s x)
+class GDoJobRec (t :: Bool) s a where
+  gDoJobRec :: forall g (x :: Type) . Applicative g => Proxy t -> (a -> g a) -> s x -> g (s x)
+
+instance (a ~ b) => GDoJobRec 'True (K1 R a) b where
+  gDoJobRec _ f (K1 x) = K1 <$> f x
+
+instance GDoJobRec 'False (K1 R a) b where
+  gDoJobRec _ _ (K1 x) = K1 <$> pure x
+
+newtype MyText = MyText { unText :: Text } deriving Show
+instance Monoid MyText where
+  mempty = MyText mempty
+  MyText t1 `mappend` MyText t2 = MyText $ t1 <> t2
 
 go2 :: IO ()
 go2 = do
-  let res :: IO (MT Int) = traverseOf (doJob @_ @Int) (pure . succ) mt
+  let res = (traverseOf (doJob @_ @MyText) (\x -> pure $ (MyText "==> ") <> x) <=<
+             traverseOf (doJob @_ @Int) (pure . succ)) mt
   putText . show =<< res
 
--- instance GDoJob (Rec0 (param p) a) a where
---   -- gparam f x = trace "recIso" $ recIso f x
---   gDoJob f x = undefined -- trace "recIso" $ recIso f x
-
-  --  class HasParam (p :: Nat) s t a b | p t a -> s, p s b -> t, p s -> a, p t -> b where
-  -- param :: Applicative g => (a -> g b) -> s -> g t
-
 -- data MT a = MT [[a]] deriving (Show, Generic)
-data MT a = MT [[Maybe a]] [[a]] a Text deriving (Show, Generic)
+-- data MT a = MT [[Maybe a]] [[a]] a [Text] deriving (Show, Generic)
+data MT a b = MT { aofa :: [[Maybe a]]
+                 , as :: [[a]]
+                 , v :: a
+                 , name :: [Text]
+                 , myText :: MyText
+                 } deriving (Show, Generic)
 
-mt :: MT Int
+mt :: MT Int [Text]
 -- mt = MT [[1 ..3 ], [10..11]]
-mt = MT [[Just 1, Just 10, Nothing]] [[1 ..3], [30 ..35]] 997 "xxx"
-  
+mt = MT [[Just 1, Just 10, Nothing]] [[1 ..3], [30 ..35]] 997 ["xxx"] (MyText "Wania")
+
 
 traverseOf :: a -> a
 traverseOf = identity
